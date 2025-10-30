40c5b8a4dbd6b5668ad3369bc0c560d243a32c75
bmeekins93 <brianmeekinswork@gmail.com>
2025-10-30 01:21:48 -0400

    Initialize Phaser game conInitialize Phaser game in main.jsfiguration

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-// Main game file stub
-// TODO: Initialize Phaser game (T0.2)
+// Main game file
+const config = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  backgroundColor: '#000000',
+  scene: {
+    preload: function () {},
+    create: function () {},
+    update: function () {}
+  }
+};
+
+const game = new Phaser.Game(config);
