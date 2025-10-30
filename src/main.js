426625a449d749a99dac72c04484e6e84cfb5393
bmeekins93 <brianmeekinswork@gmail.com>
2025-10-30 04:17:34 -0400

    Refactor game configuration afeat(s1): scaffold Phaser scenes (T1.1) - update main.jsnd scene imports

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-// Main game file
+// src/main.js
+import BootScene from './scenes/BootScene.js';
+import PreloadScene from './scenes/PreloadScene.js';
+import MainMenuScene from './scenes/MainMenuScene.js';
+import LevelScene from './scenes/LevelScene.js';
+import HUDScene from './scenes/HUDScene.js';
+import PauseScene from './scenes/PauseScene.js';
+import GameOverScene from './scenes/GameOverScene.js';
+
 const config = {
   type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
-  backgroundColor: '#000000',
-  scene: {
-    preload: function () {},
-    create: function () {},
-    update: function () {}
-  }
+  parent: 'game',
+  width: 960,
+  height: 540,
+  backgroundColor: '#101018',
+  physics: {
+    default: 'arcade',
+    arcade: { gravity: { y: 1000 }, debug: false }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: { width: 480, height: 270 },
+    max: { width: 1920, height: 1080 }
+  },
+  scene: [BootScene, PreloadScene, MainMenuScene, LevelScene, HUDScene, PauseScene, GameOverScene]
 };
 
-const game = new Phaser.Game(config);
+window.addEventListener('load', () => { new Phaser.Game(config); });
